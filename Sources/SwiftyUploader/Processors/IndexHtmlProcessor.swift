27213f5bc16646d2b8e3174bdae9db5014b97b05
Luke <kejinlu@gmail.com>
2023-07-19 18:44:26 +0800

    国际化完善

--- Sources/SwiftyUploader/Processors/IndexHtmlProcessor.swift
+++ Sources/SwiftyUploader/Processors/IndexHtmlProcessor.swift
@@ -26,20 +26,22 @@ class IndexHtmlProcessor: Processor {
             let epilogue = Bundle.module.localizedString(forKey: "EPILOGUE", value: "", table: nil)
             let upload_files = Bundle.module.localizedString(forKey: "UPLOAD_FILES", value: "", table: nil)
             let create_folder = Bundle.module.localizedString(forKey: "CREATE_FOLDER", value: "", table: nil)
+            let create_folder_tip = Bundle.module.localizedString(forKey: "CREATE_FOLDER_TIP", value: "", table: nil)
+            let uploading_tip = Bundle.module.localizedString(forKey: "UPLOADING_TIP", value: "", table: nil)
             let refresh = Bundle.module.localizedString(forKey: "REFRESH", value: "", table: nil)
-
-            let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
-            let footer = (title ?? "") + " " + (version ?? "")
+            let cancel = Bundle.module.localizedString(forKey: "CANCEL", value: "", table: nil)
             
             html = html?.replacingOccurrences(of: "%device%", with: device)
             html = html?.replacingOccurrences(of: "%header%", with: header ?? "")
             html = html?.replacingOccurrences(of: "%prologue%", with: prologue)
             html = html?.replacingOccurrences(of: "%epilogue%", with: epilogue)
-            html = html?.replacingOccurrences(of: "%footer%", with: footer)
             html = html?.replacingOccurrences(of: "%title%", with: title ?? "")
             html = html?.replacingOccurrences(of: "%upload_files%", with: upload_files)
             html = html?.replacingOccurrences(of: "%create_folder%", with: create_folder)
+            html = html?.replacingOccurrences(of: "%create_folder_tip%", with: create_folder_tip)
+            html = html?.replacingOccurrences(of: "%uploading_tip%", with: uploading_tip)
             html = html?.replacingOccurrences(of: "%refresh%", with: refresh)
+            html = html?.replacingOccurrences(of: "%cancel%", with: cancel)
         } catch {
             
         }
